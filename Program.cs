c109b54026558b14ba5e1cc74354545eb9d317fe
LX Sevaar <m.alex.ritchie@gmail.com>
2023-03-28 12:35:59 +0100

    Second and Final Commit Before Merge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("I will now merge this file with the main branch to display only this message.");
+Console.WriteLine("I will now merge this file with the main branch to display only this message and the one below.");
+
+Console.WriteLine("Hello.");
 
